a256e78eebea9cd3691619894b5c69605cd4132b
Girik1105 <girik1105@gmail.com>
2021-06-27 17:29:00 +0530

    Added Base for landing, login, signup

--- project/views.py
+++ project/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class index(TemplateView):
-    template_name = 'index.html'
+    template_name = 'landing/index.html'
